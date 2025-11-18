4ce88315e6d23ebeb9a3c168a5d9a9333466842d
Furkan Türkyılmaz <trkyilmazfurkan@gmail.com>
2025-11-18 21:59:51 +0300

    feat: Add product entity, service interface and implementation, sample data, and API launch configuration.

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ShoppingProject.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public decimal Price { get; set; }
+        public string Description { get; set; } = "";
+        public string Category { get; set; } = "";
+        public string Image { get; set; } = "";
+        public Rating Rating { get; set; } = new Rating();
+    }
+
+    public class Rating
+    {
+        public double Rate { get; set; }
+        public int Count { get; set; }
+    }
+}
