8fa1f68eee3d89e72238789d8058f01f6566f66b
Furkan Türkyılmaz <trkyilmazfurkan@gmail.com>
2025-12-15 20:48:43 +0300

    style: Adjust indentation of UnhandledExceptionBehaviour class.

--- src/Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
+++ src/Application/Common/Behaviours/UnhandledExceptionBehaviour.cs
@@ -2,61 +2,62 @@
 using Microsoft.Extensions.Logging;
 using ShoppingProject.Application.Common.Exceptions;
 
-namespace ShoppingProject.Application.Common.Behaviours;
-
-public class UnhandledExceptionBehaviour<TRequest, TResponse>
-    : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : notnull
+namespace ShoppingProject.Application.Common.Behaviours
 {
-    private readonly ILogger<UnhandledExceptionBehaviour<TRequest, TResponse>> _logger;
-
-    public UnhandledExceptionBehaviour(
-        ILogger<UnhandledExceptionBehaviour<TRequest, TResponse>> logger
-    )
+    public class UnhandledExceptionBehaviour<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull
     {
-        _logger = logger;
-    }
+        private readonly ILogger<UnhandledExceptionBehaviour<TRequest, TResponse>> _logger;
 
-    public async Task<TResponse> Handle(
-        TRequest request,
-        RequestHandlerDelegate<TResponse> next,
-        CancellationToken cancellationToken
-    )
-    {
-        try
+        public UnhandledExceptionBehaviour(
+            ILogger<UnhandledExceptionBehaviour<TRequest, TResponse>> logger
+        )
         {
-            return await next();
+            _logger = logger;
         }
-        catch (NotFoundException ex)
-        {
-            _logger.LogWarning(ex, "Entity not found in {RequestName}", typeof(TRequest).Name);
-            throw new NotFoundException($"Entity not found in {typeof(TRequest).Name}", ex);
-        }
-        catch (BusinessRuleException ex)
-        {
-            _logger.LogWarning(
-                ex,
-                "Business rule violated in {RequestName}",
-                typeof(TRequest).Name
-            );
-            throw new BusinessRuleException(
-                $"Business rule violated in {typeof(TRequest).Name}",
-                ex
-            );
-        }
-        catch (Exception ex)
-        {
-            var requestName = typeof(TRequest).Name;
 
-            _logger.LogError(
-                ex,
-                "Unhandled exception in {Behaviour} for request {RequestName} {@Request}",
-                nameof(UnhandledExceptionBehaviour<TRequest, TResponse>),
-                requestName,
-                request
-            );
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken
+        )
+        {
+            try
+            {
+                return await next();
+            }
+            catch (NotFoundException ex)
+            {
+                _logger.LogWarning(ex, "Entity not found in {RequestName}", typeof(TRequest).Name);
+                throw new NotFoundException($"Entity not found in {typeof(TRequest).Name}", ex);
+            }
+            catch (BusinessRuleException ex)
+            {
+                _logger.LogWarning(
+                    ex,
+                    "Business rule violated in {RequestName}",
+                    typeof(TRequest).Name
+                );
+                throw new BusinessRuleException(
+                    $"Business rule violated in {typeof(TRequest).Name}",
+                    ex
+                );
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
 
-            throw new UnhandledRequestException(requestName, request, ex);
+                _logger.LogError(
+                    ex,
+                    "Unhandled exception in {Behaviour} for request {RequestName} {@Request}",
+                    nameof(UnhandledExceptionBehaviour<TRequest, TResponse>),
+                    requestName,
+                    request
+                );
+
+                throw new UnhandledRequestException(requestName, request, ex);
+            }
         }
     }
 }
