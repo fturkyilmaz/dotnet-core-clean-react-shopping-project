88058cef60fc1dbe91f0b7796f71536f480f0c16
Furkan Türkyılmaz <trkyilmazfurkan@gmail.com>
2025-12-17 09:49:05 +0300

    feat(cart): update entity ownerId property and DTO

--- src/Application/DTOs/CartDto.cs
+++ src/Application/DTOs/CartDto.cs
@@ -1,3 +1,10 @@
 namespace ShoppingProject.Application.DTOs;
 
-public record CartDto(int Id, string Title, decimal Price, string Image, int Quantity);
+public record CartDto(
+    int Id,
+    string Title,
+    decimal Price,
+    string Image,
+    int Quantity,
+    string OwnerId
+);
