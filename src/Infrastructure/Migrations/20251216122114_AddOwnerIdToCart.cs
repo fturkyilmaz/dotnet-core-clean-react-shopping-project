7a94d4e59c33dcd76ac271d779f571d092f8ff03
Furkan Türkyılmaz <trkyilmazfurkan@gmail.com>
2025-12-17 09:50:56 +0300

    feat(migration): add ownerId column to Cart table

---
+++ src/Infrastructure/Migrations/20251216122114_AddOwnerIdToCart.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingProject.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOwnerIdToCart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Carts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Carts");
+        }
+    }
+}
